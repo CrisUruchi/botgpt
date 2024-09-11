f442f94f987422dcefee7fe05068be4952e50ec1
CrisUruchi <cristhian.uruchi@gmail.com>
2024-09-11 12:09:07 -0400

    puerto 3004

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { ConfigService } from '@nestjs/config'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('APP_PORT') || 3000;
-  await app.listen(3000);
+  const port = configService.get<number>('APP_PORT') || 3004;
+  await app.listen(3004);
   console.log('app running on port :', port);
 }
 bootstrap();
